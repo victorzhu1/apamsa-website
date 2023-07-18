cbda88ee4b568e2df9da04c24c7ef68546b59e04
victorzhu1 <victor.zhu49@gmail.com>
2023-07-18 16:03:14 -0400

    Test vercel sync

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ export function Home () {
         <div className='home-container shadow mb-16'>
             <div className='hero-container flex justify-center items-center h-screen text-blue-900'>
                 <div className='home-info mr-48 font-sans'>
-                    <h1 className="text-7xl font-bold ">UPenn APAMSA change test</h1>
+                    <h1 className="text-7xl font-bold ">UPenn APAMSA change test2</h1>
                     <p className="text-xl font-bold">placeholdertext</p>
                     <div className="home-info-buttons">
                         <a role="button" href="/about" className="inline-block cursor-pointer rounded-md bg-sky-600 px-4 py-3 
