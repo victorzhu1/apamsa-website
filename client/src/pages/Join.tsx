b9590f0f5be80543bac82d6a9ce6988d08a83c70
victorzhu1 <victor.zhu49@gmail.com>
2023-08-07 23:02:19 -0400

    join and gallery flex added

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -23,14 +23,15 @@ export function Join() {
                     background: 'rgba(39, 159, 255, 0.6)',
                     }}
                 >
-                    <div className="join-text-container w-3/4 md:w-2/3 px-8 md:px-16 my-6 py-6 md:py-16 bg-slate-100 bg-opacity-75 border-2 border-slate-800">
-                        <h1 className="text-slate-900 font-bold text-2xl md:text-5xl mb-2 md:mb-16">Interested in joining?</h1>
-                        <p className="text-md md:text-xl mb-2 md:mb-8">
+                    <div className="join-text-container md:flex flex-col md:space-y-4 items-center w-3/4 md:w-2/3 px-8 md:px-16 my-6 py-6 md:py-16 bg-slate-100 bg-opacity-75 
+                    border-2 border-slate-800">
+                        <h1 className="text-slate-900 font-bold text-2xl md:text-5xl mb-2">Interested in joining?</h1>
+                        <p className="text-md md:text-xl mb-2">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                             ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                             laboris nisi ut aliquip ex ea commodo consequat.
                         </p>
-                        <a role="button" href="https://www.google.com" target="_blank" className="inline-block border-2 border-slate-800 cursor-pointer px-4 py-3 
+                        <a role="button" href="https://www.google.com" target="_blank" className="inline-block md:w-2/3 border-2 border-slate-800 cursor-pointer px-4 py-3 
                         text-center text-md font-semibold uppercase text-slate-800 transition duration-200 ease-in-out 
                         hover:bg-opacity-25 hover:bg-slate-900">
                             Sign up form!
