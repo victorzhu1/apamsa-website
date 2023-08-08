f643deb74015d0bdd2c4bc6ce253819f838ee51b
victorzhu1 <victor.zhu49@gmail.com>
2023-08-07 22:35:42 -0400

    footer tablet friendly

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
   };
   
   return (
-    <footer className='footer-container w-screen md:px-64 py-8 md:py-16 bg-slate-700 text-white font-oswald'>
+    <footer className='footer-container w-screen md:px-32 lg:px-64 py-8 md:py-16 bg-slate-700 text-white font-oswald'>
         <div className='flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0'>
             <div className='logo-container flex flex-col items-center justify-center'>
               <img src="/images/image-logo-small.jpg" className="h-16" alt="Logo" />
