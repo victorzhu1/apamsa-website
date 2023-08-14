4daa60d7e52679a07e3374f70046e1ff489bea94
victorzhu1 <victor.zhu49@gmail.com>
2023-08-14 15:55:03 -0400

    make server files .ts. update footer button

---
+++ server/models/PostModel.ts
@@ -0,0 +1,24 @@
+
+const postSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Enter post title:"],
+        },
+        body: {
+            type: String,
+            required: [true, "Enter post body:"],
+        },
+        date: {
+            type: String,
+            required: [true, "Enter post date:"],
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
